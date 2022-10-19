5508fedcb3c5fb45bc05ef6f677f317f0b3dd2eb
Avin Divakara <divakaraavin@gmail.com>
2022-10-19 20:43:30 +0530

    Added short URL retrieval API & Added changes to other APIs

--- api/index.js
+++ api/index.js
@@ -44,6 +44,7 @@ app.post('/create_url', async (req, res) => {
     const url = req.body.url;
     const url_name = req.body.name;
     let is_available = false;
+    let is_valid = false;
     let error = null;
     try {
          await axios.get("http://localhost:3000/api/available_name", {
@@ -54,6 +55,14 @@ app.post('/create_url', async (req, res) => {
             response.status !== 200 ? error = response.status : error = null;
             if(error===null) is_available = response.data.is_available;
         });
+        await axios.post("http://localhost:3000/api/check_name",
+            {
+                name: url_name,
+            }
+        ).then((response) => {
+            response.status !== 200 ? error = response.status : error = null;
+            if(error===null) is_valid = response.data.is_valid;
+        });
     }
     catch (err) {
         console.log(err);
@@ -61,11 +70,13 @@ app.post('/create_url', async (req, res) => {
     }
     if (error===null) {
         try{
-            if (is_available){
-                const create_res = await appwrite.createDocument(url, url_name);
-                res.status(201).json({data: create_res});
-            } else {
+            if (is_available && is_valid){
+                await appwrite.createDocument(url, url_name);
+                res.status(201).json({creation: "success"});
+            } else if(!is_available) {
                 res.status(409).json({error: "Name already taken!"});
+            } else if(!is_valid){
+                res.status(406).json({error: "Name is not in the requested format!"})
             }
         }
         catch (err) {
@@ -74,6 +85,30 @@ app.post('/create_url', async (req, res) => {
         }
     } else {
         res.status(504).json({error: "Could not create link!"});
+    }
+});
+
+// Short URL details retrieval
+app.get('/url_details', async (req, res) => {
+    const url_name = req.query.name;
+    try {
+        if (url_name){
+            const search_res = await appwrite.listDocuments(url_name);
+            const data = search_res.documents[0];
+            const url = data.url;
+            if (search_res.total>0){
+                res.status(200).json({url: url});
+            } else {
+                res.status(404).json({error: "Not found!"});
+            }
+        }
+        else {
+            res.status(404).json({error: "Not found!"});
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(504).json({error: "Server Error!"});
     }
 });
 
