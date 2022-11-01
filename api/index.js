175fa80a69b23907c4395a7b61952b1e71e743a5
Avin Divakara <divakaraavin@gmail.com>
2022-11-01 17:36:51 +0530

    Corrected error in API

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
-import { AppwriteApi } from '../model/appwrite/appwriteApi';
-import { DataHandler } from '~/model/dataHandler';
+import { DataHandler } from '../model/dataHandler';
 const bodyParser = require('body-parser');
 // const request = require('request');
 const axios = require('axios');
 const app = require('express')();
 
 
-const appwrite = new AppwriteApi();
 const dataHandler = new DataHandler();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 // Checks for available short URL names
 app.get('/available_name', async (req, res) => {
@@ -34,8 +32,9 @@ app.post('/check_name', async (req, res) => {
 // Creates new short URL
 app.post('/create_url', async (req, res) => {
     const url = req.body.url;
+    const auto_generate = req.body.auto_generate;
     const url_name = req.body.name;
-    const status = await dataHandler.create_url(url, url_name);
+    const status = await dataHandler.create_url(url, url_name, auto_generate);
     if (status !== null) {
         res.status(200).json({ created: status });
     } else {
--- model/dataHandler.js
+++ model/dataHandler.js
@@ -1,5 +1,8 @@
-import { AppwriteApi } from '../appwrite/appwriteApi';
+import { AppwriteApi } from '../model/appwrite/appwriteApi';
+import crypto from 'crypto';
+// import { argon2d, argon2i } from 'argon2';
 
+const appwrite = new AppwriteApi();
 export class DataHandler {
   // Checks validity of URL name
   valid_name(name) {
@@ -14,7 +17,7 @@ export class DataHandler {
   async available_name(name) {
     try {
       if (this.valid_name(name)) {
-        const search_res = await appwrite.listDocuments(url_name);
+        const search_res = await appwrite.listDocuments(name);
         const is_available = search_res.total < 1;
         if (is_available) return true;
         else return false;
@@ -29,23 +32,53 @@ export class DataHandler {
     }
   }
   // Creates new short URL
-  async create_url(url, url_name) {
-    try {
-      if (url && url_name) {
-        const is_valid = this.valid_name(url_name);
-        if (!is_valid) return false;
-        const is_available = await this.available_name(url_name);
-        if (is_available && is_valid) {
-          await appwrite.createDocument(url, url_name);
-          return true;
+  async create_url(url, url_name, auto_generate) {
+    let is_available = false;
+    let is_valid = true;
+    if (!auto_generate) {
+      try {
+        if (url && url_name) {
+          is_valid = this.valid_name(url_name);
+          if (!is_valid) return false;
+          is_available = await this.available_name(url_name);
         } else {
           return false;
         }
+      }
+      catch (err) {
+        console.log(err);
+        return null;
+      }
+    } else {
+      try {
+        if (url) {
+            console.log(url);
+            let hash = await this.hash_url(url);
+            console.log(hash);
+            is_available = await this.available_name(hash);
+            console.log(is_available);
+            url_name = hash;
+            if (!is_available) {
+              return url_name;
+            }
+        } else {
+          return false;
+        }
+      }
+      catch (err) {
+        console.log(err);
+        return null;
+      }
+    }
+    try {
+      console.log(is_available, is_valid);
+      if (is_available && is_valid) {
+        await appwrite.createDocument(url, url_name);
+        return url_name;
       } else {
         return false;
       }
-    }
-    catch (err) {
+    } catch (err) {
       console.log(err);
       return null;
     }
@@ -71,5 +104,10 @@ export class DataHandler {
       console.log(err);
       return null;
     }
+  }
+
+  async hash_url(url) {
+    const hash = await crypto.createHash('sha1', url).digest('hex');
+    return hash;
   }
 }
